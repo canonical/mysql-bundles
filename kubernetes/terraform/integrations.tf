03767fc262449a1a24b3809680b896734425c187
Soundarya03 <soundarya1103@gmail.com>
2026-07-08 15:36:47 +0530

    fix: k8s lint

--- kubernetes/terraform/integrations.tf
+++ kubernetes/terraform/integrations.tf
@@ -124,7 +124,7 @@ resource "juju_offer" "mysql_client" {
   model_uuid = var.model
   count      = local.mysql_client_offered ? 1 : 0
 
-  name              = var.mysql_client_offer
-  application_name  = module.mysql_server.app_name
-  endpoints         = [module.mysql_server.provides.database]
+  name             = var.mysql_client_offer
+  application_name = module.mysql_server.app_name
+  endpoints        = [module.mysql_server.provides.database]
 }
